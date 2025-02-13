c8f9c13ea104958e851f01ece9ea148389648a15
TaliyIvanov <pavlovv1993@gmail.com>
2025-02-13 16:55:46 +0300

    250213

---
+++ 02.1.ADT_Linked list.py
@@ -0,0 +1,31 @@
+"""
+В разработке
+"""
+class MyLinkedList:
+    def __init__(self):
+        self.l = []
+    # добавить элемент
+    def add(self):
+        pass
+    # удалить элемент
+    def remove(self):
+        pass
+    # Найти элемент
+    def find(self):
+        pass
+    # подсчет кол-ва элементов
+    def count(self):
+        counter = 0
+        pass
+    # Добавить в конец
+    def push_back(self):
+        pass
+    # Добавить в начало
+    def push_front(self):
+        pass
+    # Удалить первый
+    def pop_front(self):
+        pass
+    # Удалить последний
+    def pop_back(self):
+        pass
